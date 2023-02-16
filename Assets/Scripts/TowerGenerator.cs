e13e6a26801e55b7b6b1531e8075ba87a1f1ff37
Cdunn19 <40079476+Cdunn19@users.noreply.github.com>
2023-02-16 13:34:56 -0600

    Cleaned up TowerGenerator.cs
    Removed a lot of unnecessary lines, decided what needs to be worked on next

--- Assets/Scripts/TowerGenerator.cs
+++ Assets/Scripts/TowerGenerator.cs
@@ -9,22 +9,12 @@ public class TowerGenerator : MonoBehaviour
     public GameObject walls;
     public GameObject glass;
 
-    public int glassOffset = -1;
-    public int floors = -1;
-    public int height = 0;
-    public int baseWidth = -1;
-    public int baseLength = -1;
-    public int floorHeightMod = -1;
-
-    public float wallColorR = -1f;
-    public float wallColorG = -1f;
-    public float wallColorB = -1f;
-    public float wallColorA = 1f;
-
-    public float glassColorR = -1f;
-    public float glassColorG = -1f;
-    public float glassColorB = -1f;
-    public float glassColorA = -1f;
+    public int glassOffset;
+    public int floors;
+    public int towerHeight;
+    public int baseWidth;
+    public int baseLength;
+    public int floorHeight;
 
     public Color wallColor;
     public Color glassColor; 
@@ -33,102 +23,73 @@ public class TowerGenerator : MonoBehaviour
 
     public Shader transparentShader;
 
-    public class cube
+    /*
+     * TODO:
+     * 
+     * Utilize Cube class to cut down some of the heavy looping, using xyz coords
+     * 
+     * Fix floors not rendering -- issue with order of if statements (and rendering in general needs rewrite).
+     */
+
+
+
+    private class Cube
     {
         public Color cubeColor;
         public Shader cubeShader;
         public bool isRendered = false;
         public bool isGlass = false;
-
-
-    }
-
-    public float rollValue(float value, float lower, float upper){
-        if(value < 0)
-        {
-            return Random.Range(lower, upper);
-        }
-        else
-        {
-            return value;
-        }
     }
 
     void Start()
     {
         transparentShader = Shader.Find("Transparent/Diffuse");
 
-        wallColorR = rollValue(wallColorR, 0f, 1f);
-        wallColorG = rollValue(wallColorG, 0f, 1f);
-        wallColorB = rollValue(wallColorB, 0f, 1f);
+        wallColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f); 
+        glassColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(.05f, .5f)); 
 
-        glassColorR = rollValue(glassColorR, 0f, 1f);
-        glassColorG = rollValue(glassColorG, 0f, 1f);
-        glassColorB = rollValue(glassColorB, 0f, 1f);
-        if(glassColorA < 0)
-        {
-            glassColorA = Random.Range(.05f, .5f);
-        }
+        floorHeight = Random.Range(2, 5);
+        floors = (Random.Range(2, 40));
+        towerHeight = ((floors * (floorHeight + 1)) + 1);
 
-        wallColor = new Color(wallColorR, wallColorG, wallColorB, wallColorA);
-        glassColor = new Color(glassColorR, glassColorG, glassColorB, glassColorA);
+        Debug.Log("floors = " + floors + ", floorHeight = " + floorHeight + ", height = " + towerHeight);
 
-        if (floorHeightMod < 2)
-        {
-            floorHeightMod = Random.Range(2, 5);
-        }
-        if (floors < 1)
-        {
-            floors = (Random.Range(2, 40));
-        }
-        height = ((floors * (floorHeightMod + 1)) + 1);
-        Debug.Log("floors = " + floors + ", floorHeightMod = " + floorHeightMod + ", height = " + height);
-        if (baseWidth < 1)
-        {
-            baseWidth = Random.Range(25, 50);
-        }
-        Debug.Log("baseWidth = " + baseWidth);
-        if (baseLength < 1)
-        {
-            baseLength = Random.Range(25, 50);
-        }
-        Debug.Log("baseLength = " + baseLength);
-        if (glassOffset < 0)
-        {
-            glassOffset = Random.Range(0, 2);
-        }
+        baseWidth = Random.Range(25, 50);
+        baseLength = Random.Range(25, 50);
 
-        cube[,,] cubeGrid = new cube[height, baseWidth, baseLength];
+        Debug.Log("baseWidth = " + baseWidth + ", baseLength = " + baseLength);
 
-        for (int heightCount = 0; heightCount < height; heightCount++)
+        Cube[,,] cubeGrid = new Cube[towerHeight, baseWidth, baseLength];
+
+        for (int heightCount = 0; heightCount < towerHeight; heightCount++) //initialize all possible cubes of the tower, including interior
         {
             for (int widthCount = 0; widthCount < baseWidth; widthCount++)
             {
                 for (int lengthCount = 0; lengthCount < baseLength; lengthCount++)
                 {
-
-                    cubeGrid[heightCount, widthCount, lengthCount] = new cube();
-
+                    cubeGrid[heightCount, widthCount, lengthCount] = new Cube();
                 }
             }
-
         }
 
-        for (int heightCount = 0; heightCount < height; heightCount++)
+        for (int heightCount = 0; heightCount < towerHeight; heightCount++)
         {
             for (int widthCount = 0; widthCount < baseWidth; widthCount++)
             {
                 for (int lengthCount = 0; lengthCount < baseLength; lengthCount++)
                 {
-                    if ((heightCount == 0 || heightCount == height - 1) || (lengthCount == 0 || lengthCount == baseLength - 1) || (widthCount == 0 || widthCount == baseWidth - 1)) //only outside blocks(including top & bottom)
+                    if ((heightCount == 0 || heightCount == towerHeight - 1) || //Ignores all cubes not on the outside of the array
+                        (lengthCount == 0 || lengthCount == baseLength - 1) ||
+                        (widthCount == 0 || widthCount == baseWidth - 1)) 
                     {
-                        if(heightCount%(floorHeightMod + 1) == 0) { //Render solid Floors according to floorHeightMod, set current cubeColor to wallColor
+                        //This if statement needs to be before the previous if statement to function, on to-do list
+                        if(heightCount%(floorHeight + 1) == 0) { //Render solid Floors according to floorHeight, set current cubeColor to wallColor
                             Tower = Instantiate(cubePrefab, new Vector3(widthCount, heightCount, lengthCount), Quaternion.identity, walls.transform);
                             cubeGrid[heightCount, widthCount, lengthCount].cubeColor = wallColor;
                             Tower.GetComponent<Renderer>().material.color = wallColor;
                         }
                         else
-                        {   //Render Windows between floors (everything on the outside that isn't considered to be part of a floor), sets current cubeColor to defaultGlassColor & sets cubeShader to transparentShader
+                        {   //Render Windows between floors (everything on the outside that isn't considered to be part of a floor)
                             Tower = Instantiate(cubePrefab, new Vector3(widthCount, heightCount, lengthCount), Quaternion.identity, walls.transform);
                             cubeGrid[heightCount, widthCount, lengthCount].cubeColor = glassColor;
                             Tower.GetComponent<Renderer>().material.color = glassColor;
